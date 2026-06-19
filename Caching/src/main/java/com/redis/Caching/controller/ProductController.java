3422bad79cf82c6a1bc21495be79b8ad12d2ca10
Ajay_00 <ajaylohith855@gmail.com>
2026-06-19 22:41:32 +0530

    updated codebase

--- Caching/src/main/java/com/redis/Caching/controller/ProductController.java
+++ Caching/src/main/java/com/redis/Caching/controller/ProductController.java
@@ -1,10 +1,47 @@
 package com.redis.Caching.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.redis.Caching.dto.ProductDto;
+import com.redis.Caching.model.Product;
+import com.redis.Caching.service.ProductService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
 
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product>getProductById(@PathVariable Long id){
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Product>createProduct(@RequestBody ProductDto request){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(productService.createProduct(request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product>updateProduct(@PathVariable Long id,
+                                                @RequestBody ProductDto productDto){
+        return ResponseEntity.ok(productService.updateProduct(id,productDto));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void>deleteProduct(@PathVariable Long id){
+        productService.deleteProductById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- Caching/src/main/java/com/redis/Caching/service/ProductService.java
+++ Caching/src/main/java/com/redis/Caching/service/ProductService.java
@@ -1,7 +1,69 @@
 package com.redis.Caching.service;
 
+import com.redis.Caching.dto.ProductDto;
+import com.redis.Caching.model.Product;
+import com.redis.Caching.repository.ProductRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProductService {
+    private final ProductRepo productRepo;
+
+    private void simulateSlowDbCall() {
+        try {
+            Thread.sleep(500);
+        }catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public List<Product> getAllProducts() {
+        log.info("----FETCHING ALL PRODCUTS from DB");
+        simulateSlowDbCall();
+        return productRepo.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        log.info("---- FETCHING PRODUCT:{} from DB",id);
+        simulateSlowDbCall();
+        return productRepo.findById(id)
+                .orElseThrow(()->new RuntimeException("Product: "+id+" not found"));
+    }
+
+    public Product createProduct(ProductDto productDto) {
+        log.info("---- Creating new product: {} in DB",productDto.getName());
+        Product product=Product.builder()
+                .name(productDto.getName())
+                .description(productDto.getDescription())
+                .category(productDto.getCategory())
+                .price(productDto.getPrice())
+                .stock(productDto.getStock())
+                .build();
+        return productRepo.save(product);
+    }
+
+    public Product updateProduct(Long id, ProductDto productDto) {
+        log.info("---- Updating product {} in DB",id);
+        Product existing=getProductById(id);
+        existing.setName(productDto.getName());
+        existing.setDescription(productDto.getDescription());
+        existing.setCategory(productDto.getCategory());
+        existing.setPrice(productDto.getPrice());
+        existing.setStock(productDto.getStock());
+        return productRepo.save(existing);
+    }
+
+    public void deleteProductById(Long id) {
+        log.info("---- DELETING product {} in DB",id);
+        productRepo.deleteById(id);
+    }
+
+
 }
